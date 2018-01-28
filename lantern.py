1065990d00370209f8ed9fb4c3dac191265a9572
Dmitry Doroshev <r.schweppes@ya.ru>
2018-01-28 22:30:00 +0300

    Initial commit

---
+++ lantern.py
@@ -0,0 +1,107 @@
+import asyncio
+from collections import defaultdict
+from functools import wraps
+
+from xtermcolor import colorize
+
+
+def cmd(code):
+    """
+    Collect commands from class methods
+    """
+    if not hasattr(cmd, 'commands'):
+        cmd.commands = defaultdict(dict)
+
+    def _cmd(fn):
+        classname = fn.__qualname__.split('.')[0]
+        cmd.commands[classname][code] = fn
+        return fn
+
+    return _cmd
+
+
+class Manageable:
+
+    async def listen(self, loop):
+        """
+        Connect to server and listen to commands
+        """
+        reader, _ = await asyncio.open_connection('127.0.0.1', 9999, loop=loop)
+        while True:
+            await self.execute_command(reader)
+
+    async def execute_command(self, reader):
+        """
+        Execute class command
+        """
+        type_, value = await self.get_tlv(reader)
+        command_code = ord(type_)
+        class_commands = cmd.commands[self.__class__.__name__]
+        if command_code in class_commands:
+            method = class_commands[command_code]
+            args = []
+            if value is not None:
+                args.append(value)
+            await method(self, *args)
+
+    async def get_tlv(self, reader):
+        """
+        Parse TLV-command and get type and value
+        """
+        type_ = await reader.read(1)
+        if type_:
+            value = None
+            length = await reader.read(2)
+            length = int.from_bytes(length, byteorder='big')
+            if length > 0:
+                value = await reader.read(length)
+            return type_, value
+
+
+class Lantern(Manageable):
+    """
+    Lantern class
+    """
+
+    def __init__(self, is_on=True, color=0xff0000):
+        self._is_on = is_on
+        self._color_rgb = color
+        self.refresh()
+
+    def refresh(self):
+        if self._is_on:
+            print(colorize('\u2B24', rgb=self._color_rgb))
+        else:
+            print('\n')
+
+    @cmd(0x12)
+    async def on(self):
+        """
+        Turn lantern on
+        """
+        self._is_on = True
+        print('Lantern is on...')
+        self.refresh()
+
+    @cmd(0x13)
+    async def off(self):
+        """
+        Turn lantern off
+        """
+        self._is_on = False
+        print('Lantern is off...')
+        self.refresh()
+
+    @cmd(0x20)
+    async def color(self, value: bytes):
+        """
+        Switch lantern color
+        """
+        value = int.from_bytes(value, byteorder='big')
+        self._color_rgb = value
+        self.refresh()
+
+
+loop = asyncio.get_event_loop()
+lantern = Lantern()
+loop.run_until_complete(lantern.listen(loop))
---
+++ server.py
@@ -0,0 +1,47 @@
+import asyncio
+
+
+class LanternServerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+        self.peername = transport.get_extra_info('peername')
+        print('Connection from', self.peername)
+
+        commands = [
+            # Turn off
+            bytearray([0x13, 0x00, 0x00]),
+            
+            # Turn on
+            bytearray([0x12, 0x00, 0x00]),
+
+            # Switch color to blue
+            bytearray([0x20, 0x00, 0x03, 0x00, 0x00, 0xff]),
+
+            # Switch color to red
+            bytearray([0x20, 0x00, 0x03, 0xff, 0x00, 0x00]),
+
+            # Switch color to green
+            bytearray([0x20, 0x00, 0x03, 0x00, 0xff, 0x00]),
+        ]
+
+        for command in commands:
+            self.transport.write(command)
+
+    def connection_lost(self, exc):
+        print('Lost connection of', self.peername)
+        self.transport.close()
+
+loop = asyncio.get_event_loop()
+coro = loop.create_server(LanternServerProtocol, '127.0.0.1', 9999)
+server = loop.run_until_complete(coro)
+
+print('Serving on', server.sockets[0].getsockname())
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
